ba0014e8e99fcc15e3721b3737eb496b55e006d0
atibayan <aemilia.tibayan@gmail.com>
2023-02-24 12:05:16 -0800

    first checkin

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const body_parser = require('body-parser')
+const PORT = 4000
+const path = require('path')
+const data = require('./data.json')
+
+app.use(body_parser.urlencoded({extended: true}))
+app.set('view engine', 'pug')
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  const projects = data.projects
+  const details = data.details
+  res.render('index', {projects, details});
+})
+
+app.get('/about', (req, res) => {
+  const details = data.details
+  res.render('about', {details});
+})
+
+app.listen(PORT, (err) => {
+  if(err) console.log(err)
+  else console.log(`Server listening on PORT ${PORT}`);
+})
