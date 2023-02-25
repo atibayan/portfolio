74af6ce859a74dd0d8a3b02a0dc473a6b54b7b4e
atibayan <aemilia.tibayan@gmail.com>
2023-02-24 22:23:05 -0800

    checkin portfolio

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const body_parser = require('body-parser')
-const PORT = 4000
+const PORT = 3000
 const path = require('path')
 const data = require('./data.json')
 
@@ -18,6 +18,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   const details = data.details
   res.render('about', {details});
+})
+
+app.get('/project/:id', (req, res) => {
+  const project = data.projects[req.params.id]
+  const details = data.details  
+  res.render('project', {project, details})
 })
 
 app.listen(PORT, (err) => {
